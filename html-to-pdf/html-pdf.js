b396ae8b8f9a924bb42806fb62a27f3adb84a8fb
James Zhuo <james.zhuo@smokeball.com>
2023-10-12 11:24:50 +1100

    restructured files/folders a bit

--- html-to-pdf/html-pdf.js
+++ html-to-pdf/html-pdf.js
@@ -5,7 +5,8 @@ var invoiceHtml = readInvoiceHtmlFile()
 var options = { format: 'Letter' };
 
 // Notes: Although this looks like it mostly work for what we need, the library has been deprecated
-// and is not maintained https://www.npmjs.com/package/html-pdf?activeTab=readme
+// and is not maintained anymore https://www.npmjs.com/package/html-pdf?activeTab=readme, the reason
+// appears to be that PhantomJS got deprecated long time ago https://github.com/marcbachmann/node-html-pdf
 pdf.create(invoiceHtml, options).toFile('./invoice-html-pdf.pdf', function(err, res) {
   if (err) return console.log(err);
   console.log(res);
--- html-to-pdf/read-invoice-html-file.js
+++ html-to-pdf/read-invoice-html-file.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const readInvoiceHtmlFile = () => {
-  const invoiceHtml = fs.readFileSync('./html-to-pdf/invoice-preview-4.html', 'utf8');
+  const invoiceHtml = fs.readFileSync('./invoice.html', 'utf8');
   return invoiceHtml;
 }
 
